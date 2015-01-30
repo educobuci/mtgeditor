9153b808591b7130783962b8809e3a861292ac08
Eduardo Cobuci <educobuci@gmail.com>
2015-01-30 19:33:15 -0200

    Add comments

--- app/assets/javascripts/grid.js
+++ app/assets/javascripts/grid.js
@@ -1,72 +1,84 @@
 (function($){
+  // Constants definition
   var UP_KEY = 38;
   var DOWN_KEY = 40;
   var SELECTED_CLASS = "selected";
+  var SELECTED_SELECTOR = "." + SELECTED_CLASS;
   
+  // Grid Plugin definition
   $.fn.grid = function(options){
+    
+    // References
     var plugin = $.fn.grid;
     var $element = this;
     
+    // Settings
     var settings = $.extend({
       tag: "li",
     }, options );
     
+    // Grid item mouse over event
     this.find(settings.tag).on("mouseover", function(event){
       if ($element.is(":focus") && plugin.isMouseDown === 1) {
-        $element.find("." + SELECTED_CLASS).removeClass(SELECTED_CLASS);
+        $element.find(SELECTED_SELECTOR).removeClass(SELECTED_CLASS);
         $(this).addClass(SELECTED_CLASS);
       }
     });
     
+    // Grid item mouse over event
     this.find(settings.tag).on("mousedown", function(event){
-        $element.find("." + SELECTED_CLASS).removeClass(SELECTED_CLASS);
+        $element.find(SELECTED_SELECTOR).removeClass(SELECTED_CLASS);
         $(this).addClass(SELECTED_CLASS);
     });
     
+    // Grid mouse down state control
     if (typeof(plugin.isMouseDown)==="undefined") {
       plugin.isMouseDown = 0;
     }
     
+    // Grid mouse down event
     this.mousedown(function(event){
       $(this).focus();
       event.preventDefault();
       ++plugin.isMouseDown;
     });
     
+    // Grid mouse up event
     this.mouseup(function(){
       plugin.isMouseDown = 0;
       console.log("ronaldo", plugin.isMouseDown);
     });
     
+    // Global mouse up event
     $("body").mouseup(function(){
       if(plugin.isMouseDown)
       {
         plugin.isMouseDown = 0;
       }      
     });
-
+    
+    // Grid up/down arrow keyboard events
     this.keydown(function( event ) {
       if (event.which === UP_KEY || event.which === DOWN_KEY && this.find(settings.tag).length > 0) {
         var selectedElement = null;
         
-        if (this.find("." + SELECTED_CLASS).length === 0) {
+        if (this.find(SELECTED_SELECTOR).length === 0) {
           selectedElement = this.find(settings.tag)[0];
         }
         else if (event.which === DOWN_KEY) {
-          selectedElement = this.find("." + SELECTED_CLASS).next();
+          selectedElement = this.find(SELECTED_SELECTOR).next();
         }
         else if (event.which === UP_KEY) {
-          selectedElement = this.find("." + SELECTED_CLASS).prev();
+          selectedElement = this.find(SELECTED_SELECTOR).prev();
         }
         
         if (selectedElement.length === 0) {
-          this.find("." + SELECTED_CLASS)
+          this.find(SELECTED_SELECTOR)
         }
         else
         {
-          this.find("." + SELECTED_CLASS).removeClass(SELECTED_CLASS);
+          this.find(SELECTED_SELECTOR).removeClass(SELECTED_CLASS);
         }
-        
         
         $(selectedElement).addClass(SELECTED_CLASS);
       }
